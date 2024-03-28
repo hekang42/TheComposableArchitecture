6299d767d53d85634d43af2950b09d2a635bfb6d
hekang42 <glipung@naver.com>
2024-03-28 17:36:37 +0900

    Integrating into the app

--- TCASampleApp/TCASampleAppApp.swift
+++ TCASampleApp/TCASampleAppApp.swift
@@ -10,12 +10,14 @@ import ComposableArchitecture
 
 @main
 struct TCASampleAppApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            CounterView(
-                store: Store(initialState: CounterFeature.State(), reducer: {
-                    CounterFeature() }
-                            ))
+            CounterView(store: TCASampleAppApp.store)
         }
     }
 }
