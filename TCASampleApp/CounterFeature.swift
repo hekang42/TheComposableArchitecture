3544380691afce908549af4210cdc2788ea121a6
hekang42 <glipung@naver.com>
2024-04-01 15:18:45 +0900

    managing a timer

--- TCASampleApp/CounterFeature.swift
+++ TCASampleApp/CounterFeature.swift
@@ -16,6 +16,7 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
     
     enum Action {
@@ -23,7 +24,11 @@ struct CounterFeature {
         case factButtonTapped
         case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+    
+    enum CancelID { case timer }
     
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -49,6 +54,24 @@ struct CounterFeature {
                 return .none
                 
             case .incrementButtonTapped:
+                state.count += 1
+                state.fact = nil
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
+            case .timerTick:
                 state.count += 1
                 state.fact = nil
                 return .none
